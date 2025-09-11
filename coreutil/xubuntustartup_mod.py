8d02e21fbfef7774e7911bb2ecdc5dfa31700d2e
minqwq <minqwq723897@outlook.com>
2025-09-12 03:27:56 +0800

    update v2 startup screen

--- coreutil/xubuntustartup_mod.py
+++ coreutil/xubuntustartup_mod.py
@@ -18,14 +18,15 @@ def animate_dots(stdscr):
     
     # 居中显示 "Xubuntu 25.04"
     # center_text(stdscr, height // 2 - 1, "Xubuntu 25.04")
-    center_text(stdscr, height // 2 - 3, "Codename \"The Scarlet of Devil\"")
-    center_text(stdscr, height // 2 - 2, "PY OS Improved 1.6.1")
+    center_text(stdscr, height // 2 - 4, "Codename \"Hata no Kokoro\"")
+    center_text(stdscr, height // 2 - 3, "VoidFlan Project II")
+    center_text(stdscr, height // 2 - 2, "Beta 2 Sep. Release")
     
     # 计算四个点的起始位置（居中）
     dots_x = (width - 7) // 2  # 4个点 + 3个空格 = 7字符宽度
     
     # 在四个点下面加一行字
-    center_text(stdscr, height // 2 + 2, "(C) Flandre Studio 2024--2025")
+    center_text(stdscr, height // 2 + 2, "(C) Flandre Studio 2024--2026")
     center_text(stdscr, height // 2 + 3, "(C) 0x1c Studio 2022--2023")
     
     try:
