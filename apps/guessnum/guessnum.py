ed955ce5a606fecf15753b45f88a993d58cf46b2
piotrekgelert <piotrekgelert@gmail.com>
2026-05-14 16:04:34 +0100

    small refactor of guessnum game

--- apps/guessnum/guessnum.py
+++ apps/guessnum/guessnum.py
@@ -1,25 +1,18 @@
 import random
+import sys
 
 print("Guess number v0.1R2 by minqwq and Copilot")
 print("1: max20\n2: max60\n3: max100\n4: max200\n5: max400\n6: max800\n7: max1600")
 
+diffs = ("1", "2", "3", "4", "5", "6", "7")
+numRange = (20, 60, 100, 200, 400, 800, 1600)
+
 diff = input("Select a difficulty: ")
-if diff == "1":
-    numRangeMax = 20
-elif diff == "2":
-    numRangeMax = 60
-elif diff == "3":
-    numRangeMax = 100
-elif diff == "4":
-    numRangeMax = 200
-elif diff == "5":
-    numRangeMax = 400
-elif diff == "6":
-    numRangeMax = 800
-elif diff == "7":
-    numRangeMax = 1600
-else:
-    exit()
+if diff not in diffs or diff == "":
+    sys.exit()
+
+numRangeMax = dict(zip(diffs, numRange))[diff]
+
 
 randomResult = random.randint(0, numRangeMax)
 looping = True
@@ -28,13 +21,17 @@ print("HELP:\nexit:exit\nType some number to guess")
 
 while looping:
     correctNum = randomResult
-    guessing = int(input("INPUT AREA > "))  # Convert input to an integer
-    if guessing > correctNum:
-        print("Number too high")
-    elif guessing < correctNum:
-        print("Number too low")
-    elif guessing == correctNum:
-        print("You win~")
-        exit()
-    elif guessing == "exit":
-        exit()
+    guess = input("INPUT AREA > ")
+    ln = len(guess)
+    if ln and guess == 'exit':
+        sys.exit()
+    if ln and guess.isnumeric():
+        guessing = int(guess)  # Convert input to an integer
+        
+        if guessing > correctNum:
+            print("Number too high")
+        elif guessing < correctNum:
+            print("Number too low")
+        elif guessing == correctNum:
+            print("You win~")
+            sys.exit()
